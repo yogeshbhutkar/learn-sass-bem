f9d77b13a607dfe71b879aa73d003aca2caf91f9
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-11-19 16:26:20 +0530

    feat: added config to generate map file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const { src, dest, watch, series } = require( 'gulp' );
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
 const cleanCSS = require( 'gulp-clean-css' );
+const sourcemaps = require( 'gulp-sourcemaps' );
 
 function buildStyles() {
 	return src( 'styles/**/*.scss' )
+		.pipe( sourcemaps.init() )
 		.pipe( sass() )
 		.pipe( cleanCSS() )
+		.pipe( sourcemaps.write( '.' ) )
 		.pipe( dest( 'dist' ) );
 }
 
